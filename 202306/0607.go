14d7daa0a0fd3a65d0552931df1bfffd744ff24d
WildCat0U0 <2443133871@qq.com>
2023-06-07 17:00:47 +0800

    equal-row-and-column-pairs

---
+++ 202306/0607.go
@@ -0,0 +1,18 @@
+package main
+
+import "sort"
+
+func miceAndCheese(reward1 []int, reward2 []int, k int) int {
+	n := len(reward1)
+	differences := make([]int, n) //map[key]value
+	res := 0
+	for i := 0; i < n; i++ {
+		res += reward2[i]
+		differences[i] = reward1[i] - reward2[i]
+	}
+	sort.Ints(differences)
+	for i := 1; i <= k; i++ {
+		res += differences[n-i]
+	}
+	return res
+}
